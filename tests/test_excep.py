0cc5fd1332efa09028113789c90caa2b9bb5bd63
sergey <szaprudnov@hotmail.com>
2023-04-06 13:09:03 +0300

    add method test_item_py_one

--- tests/test_excep.py
+++ tests/test_excep.py
@@ -2,3 +2,7 @@
 import pytest
 from src.phone import Item
 from src.excep import InstantiateCSVError
+
+def test_item_py_one():
+    with pytest.raises(FileNotFoundError):
+        Item.instantiate_from_csv('../src/itemsФ.csv')
